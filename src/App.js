a1b2d91df84392dcf295b4de717ec67b5a5c7b6d
Timelement <timeexe@gmail.com>
2021-10-31 21:50:03 -0700

    attempt to rework first component in the content

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { AppBar, Container, Box, Toolbar, TableFooter, Grid } from '@mui/materia
 
 function App() {
   return (
-    <Container className="App">
-      <header className="App-header">
+    <Container className="App homePageContainer">
+      {/* <header className="App-header">
         <AppBar color="default" class="nav-bar">
 
           <Toolbar>
@@ -16,22 +16,39 @@ function App() {
           </Toolbar>
 
         </AppBar>
-      </header>
+      </header> */}
 
       <Container>
-        <Container class="home-page-content">
-          <img src='https://fantasyinmotion.files.wordpress.com/2014/01/dark-soul-witch-fantasy-353840.jpg' />
-        </Container>
-        <Container class="home-page-content">
+
+
+
+        {/* <Container class="home-page-content">
           <img src="https://static2.cbrimages.com/wordpress/wp-content/uploads/2020/08/dungeons-and-dragons-bard-2.jpg?q=50&fit=crop&w=943&h=500&dpr=1.5" />
-        </Container>
-        <Container class="home-page-content">
+          </Container>
+          <Container class="home-page-content">
           <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2020/01/paladin-cover.jpg?q=50&fit=contain&w=943&h=500&dpr=1.5" />
-        </Container>
+        </Container> */}
       </Container>
 
+      <div className="heroContainer">
 
-      <Container>
+        <div className="content">
+          <h2 className="title">Dummy thiccc content</h2>
+          <h4 className="subtitle">Caked up in 4k</h4>
+          <div className="buttons">
+            <a href="" className="btn">Pics</a>
+            <a href="" className="btn">Vids</a>
+          </div>
+        </div>
+        <div className="imageContainer">
+          <img src="https://i.pinimg.com/originals/b5/fb/ab/b5fbabd0a353494b926e1014e46599bc.png" />
+        </div>
+        {/* <img src='https://fantasyinmotion.files.wordpress.com/2014/01/dark-soul-witch-fantasy-353840.jpg' /> */}
+      </div>
+
+
+
+      {/* <Container>
         <Grid container rowspacing={4} columspacing={0}>
         <Container class="home-page-content2">
           <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afb86caa-39ba-4a91-ba30-89b096bb4c5b/dd2hlhj-1cfe0289-138c-4605-ae62-de8b83d5bc95.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FmYjg2Y2FhLTM5YmEtNGE5MS1iYTMwLTg5YjA5NmJiNGM1YlwvZGQyaGxoai0xY2ZlMDI4OS0xMzhjLTQ2MDUtYWU2Mi1kZThiODNkNWJjOTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.YDjS2ShhINFNAESWwggUyB9HrNfswIJC_AK6-8pWjNw" />
@@ -46,14 +63,15 @@ function App() {
           <img src="https://cdn11.bigcommerce.com/s-7ndfb/images/stencil/1280x1280/products/61965/147255/apiijt86z__23227.1612566740.jpg?c=2" />
         </Container>
         </Grid>
-      </Container>
-      <TableFooter>
+      </Container> */}
+
+      {/* <TableFooter>
         <ul>
           <li>Dummy thicc data for the footer</li>
           <li>Dummy thicc data for the footer</li>
           <li>Dummy thicc data for the footer</li>
         </ul>
-      </TableFooter>
+      </TableFooter> */}
     </Container>
   );
 }
