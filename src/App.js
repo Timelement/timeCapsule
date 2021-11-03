182b35616a89426bf1ecf472c74f01d85dc4c21b
Timelement <timeexe@gmail.com>
2021-11-03 09:35:14 -0700

    first attempt at quadrant image layout

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <Container maxWidth={false} disableGutters={true}>
 
         <Grid container>
-          
+
           <Grid container>
 
             <Grid item xs={12}>
@@ -29,13 +29,13 @@ function App() {
                 Dummy thicc content
               </Typography>
             </Grid>
-            
+
             <Grid item xs={12}>
               <Typography variant='h4'>
                 Caked up in 4k
               </Typography>
             </Grid>
-          
+
           </Grid>
 
           <Grid container justifyContent='center'>
@@ -69,7 +69,7 @@ function App() {
       <Container maxWidth={false} disableGutters={true}>
 
         <Grid container>
-          
+
           <Grid container>
 
             <Grid item xs={12}>
@@ -77,13 +77,13 @@ function App() {
                 Dummy thicc content
               </Typography>
             </Grid>
-            
+
             <Grid item xs={12}>
               <Typography variant='h4'>
                 Caked up in 4k
               </Typography>
             </Grid>
-          
+
           </Grid>
 
           <Grid container justifyContent='center'>
@@ -117,7 +117,7 @@ function App() {
       <Container maxWidth={false} disableGutters={true}>
 
         <Grid container>
-          
+
           <Grid container>
 
             <Grid item xs={12}>
@@ -125,13 +125,13 @@ function App() {
                 Dummy thicc content
               </Typography>
             </Grid>
-            
+
             <Grid item xs={12}>
               <Typography variant='h4'>
                 Caked up in 4k
               </Typography>
             </Grid>
-          
+
           </Grid>
 
           <Grid container justifyContent='center'>
@@ -163,26 +163,58 @@ function App() {
       </Container>
 
       <Container maxWidth={false} disableGutters={true}>
-        <Grid container xs={6} justifyContent='center'>
-        <Grid item>
-          <Typography variant='h3'>
-            Spells
-          </Typography>
+        <Grid container>
+          <Container xs={6} md={6} lg={6} justifyContent='center' disableGutters={true}>
+
+            <Grid item>
+              <Typography variant='h3'>
+                Spells
+              </Typography>
+
+            </Grid>
+            <Grid item>
+              <img className='imageContainer' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afb86caa-39ba-4a91-ba30-89b096bb4c5b/dd2hlhj-1cfe0289-138c-4605-ae62-de8b83d5bc95.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FmYjg2Y2FhLTM5YmEtNGE5MS1iYTMwLTg5YjA5NmJiNGM1YlwvZGQyaGxoai0xY2ZlMDI4OS0xMzhjLTQ2MDUtYWU2Mi1kZThiODNkNWJjOTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.YDjS2ShhINFNAESWwggUyB9HrNfswIJC_AK6-8pWjNw' />
+            </Grid>
+          </Container>
+
+          <Container xs={6} md={6} lg={6} justifyContent='center' disableGutters={true}>
+            <Grid item>
+              <Typography variant='h3'>
+                Weapons and Armor
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img className='imageContainer' src='https://image.shutterstock.com/image-illustration/shield-swords-isolated-coat-arms-260nw-268312856.jpg' />
+            </Grid>
+          </Container>
+        </Grid>
+        
+        <Grid container>
+        <Container xs={6} md={6} lg={6}  justifyContent='center' disableGutters={true}>
+            <Grid item>
+              <Typography variant='h3'>
+                Classes
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img className='imageContainer' src='https://www.wargamer.com/wp-content/uploads/2021/01/dnd-5e-class-guide-main-image-adventuring-party-closeup-900x507.jpg' />
+            </Grid>
+          </Container>
+          <Container xs={6} md={6} lg={6} justifyContent='center' disableGutters={true}>
+            <Grid item>
+              <Typography variant='h3'>
+                Magic Items
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img className='imageContainer' src='https://cdn11.bigcommerce.com/s-7ndfb/images/stencil/1280x1280/products/61965/147255/apiijt86z__23227.1612566740.jpg?c=2' />
+            </Grid>
+          </Container>
 
         </Grid>
-        <Grid container>
-          <img className='imageContainer' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afb86caa-39ba-4a91-ba30-89b096bb4c5b/dd2hlhj-1cfe0289-138c-4605-ae62-de8b83d5bc95.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FmYjg2Y2FhLTM5YmEtNGE5MS1iYTMwLTg5YjA5NmJiNGM1YlwvZGQyaGxoai0xY2ZlMDI4OS0xMzhjLTQ2MDUtYWU2Mi1kZThiODNkNWJjOTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.YDjS2ShhINFNAESWwggUyB9HrNfswIJC_AK6-8pWjNw' />
-
-          </Grid>
-          
-
-
-
-      </Grid>
-
-      
-
       </Container>
+
+
 
       {/* <Container>
         <Grid container rowspacing={4} columspacing={0}>
