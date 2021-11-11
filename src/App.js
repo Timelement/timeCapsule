dd59950731c7f59572254e4488abb4fae8597b08
Timelement <timeexe@gmail.com>
2021-11-11 09:43:55 -0800

    inital creation of the footer using material ui

--- src/App.js
+++ src/App.js
@@ -214,32 +214,51 @@ function App() {
         </Grid>
       </Container>
 
+      <Grid container marginTop={20}>
+        <Grid item xs={3}>
+          <ul>
+            <li><h4>Services</h4></li>
+            <li><Link href="#">Dummy</Link></li>
+            <li><Link href="#">List</Link></li>
+            <li><Link href="#">Data</Link></li>
+          </ul>
 
-
-      {/* <Container>
-        <Grid container rowspacing={4} columspacing={0}>
-        <Container class="home-page-content2">
-          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afb86caa-39ba-4a91-ba30-89b096bb4c5b/dd2hlhj-1cfe0289-138c-4605-ae62-de8b83d5bc95.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FmYjg2Y2FhLTM5YmEtNGE5MS1iYTMwLTg5YjA5NmJiNGM1YlwvZGQyaGxoai0xY2ZlMDI4OS0xMzhjLTQ2MDUtYWU2Mi1kZThiODNkNWJjOTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.YDjS2ShhINFNAESWwggUyB9HrNfswIJC_AK6-8pWjNw" />
-        </Container>
-        <Container class="home-page-content2" >
-          <img src="https://image.shutterstock.com/image-illustration/shield-swords-isolated-coat-arms-260nw-268312856.jpg" />
-        </Container>
-        <Container class="home-page-content2" >
-          <img src="https://www.wargamer.com/wp-content/uploads/2021/01/dnd-5e-class-guide-main-image-adventuring-party-closeup-900x507.jpg" />
-        </Container>
-        <Container class="home-page-content2" >
-          <img src="https://cdn11.bigcommerce.com/s-7ndfb/images/stencil/1280x1280/products/61965/147255/apiijt86z__23227.1612566740.jpg?c=2" />
-        </Container>
         </Grid>
-      </Container> */}
 
-      {/* <TableFooter>
-        <ul>
-          <li>Dummy thicc data for the footer</li>
-          <li>Dummy thicc data for the footer</li>
-          <li>Dummy thicc data for the footer</li>
-        </ul>
-      </TableFooter> */}
+        <Grid item xs={3}>
+          <ul>
+            <li><h4>Store</h4></li>
+            <li><Link href="#">Dummy</Link></li>
+            <li><Link href="#">List</Link></li>
+            <li><Link href="#">Data</Link></li>
+          </ul>
+
+        </Grid>
+
+        <Grid item xs={3}>
+          <ul>
+            <li><h4>Resources</h4></li>
+            <li><Link href="#">Dummy</Link></li>
+            <li><Link href="#">List</Link></li>
+            <li><Link href="#">Data</Link></li>
+          </ul>
+
+        </Grid>
+
+        <Grid item xs={3}>
+          <ul>
+            <li><h4>Contact</h4></li>
+            <li><Link href="#">Dummy</Link></li>
+            <li><Link href="#">List</Link></li>
+            <li><Link href="#">Data</Link></li>
+          </ul>
+
+        </Grid>
+
+      </Grid>
+
+      
+      
     </Container>
   );
 }
